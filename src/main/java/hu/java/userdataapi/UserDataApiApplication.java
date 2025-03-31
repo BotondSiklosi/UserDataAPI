bc1bfdabb220fa93cf9fa6feb35f9dbd5ed7e648
Siki <kissiki99@gmail.com>
2025-03-31 22:59:49 +0200

    Created Project

---
+++ src/main/java/hu/java/userdataapi/UserDataApiApplication.java
@@ -0,0 +1,13 @@
+package hu.java.userdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/java/userdataapi/UserDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.java.userdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
